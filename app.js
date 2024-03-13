8147dc0af7bde7744aa6a96755611f3a477a67ee
Nhuqy <76780813+NhuqyGit@users.noreply.github.com>
2024-03-13 15:32:49 +0700

    Update app.js 1 fix

--- app.js
+++ app.js
@@ -15,18 +15,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
 io.on('connection', (socket) => {
     console.log('A user connected');
 
