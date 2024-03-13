b0c7de3d6b84e4a426cef08281b4fe9b0cebc7b7
Nhuqy <76780813+NhuqyGit@users.noreply.github.com>
2024-03-13 14:17:14 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('icecandidate', (icecandidate) => {
+        socket.broadcast.emit('icecandidate', icecandidate);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
